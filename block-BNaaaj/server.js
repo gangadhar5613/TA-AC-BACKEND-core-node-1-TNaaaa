91ff9e948a86588b07f6a1eb0f66da59c832d87a
Narapureddy Gangadhar Reddy <gangadhar.5613@gmail.com>
2020-11-09 21:48:58 +0530

    assignment complete

---
+++ block-BNaaaj/server.js
@@ -0,0 +1,9 @@
+let url = require('url');
+
+
+let parsedUrl = url.parse( `https://airindia.com/fares/calculate?from=delhi&to=detroit`,true);
+
+console.log(parsedUrl.query);
+
+console.log(parsedUrl.pathname);
+console.log(parsedUrl.protocol);
