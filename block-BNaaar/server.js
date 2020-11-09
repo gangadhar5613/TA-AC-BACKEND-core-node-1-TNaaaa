59d95be8a0302415bc377ceeab891e011fbe4e4c
Narapureddy Gangadhar Reddy <gangadhar.5613@gmail.com>
2020-11-10 00:22:37 +0530

    Refactor

--- block-BNaaar/server.js
+++ block-BNaaar/server.js
@@ -1,17 +1,17 @@
 let http = require('http');
 
-let url = require('url');
+// let url = require('url');
 
 function handleServer(req,res){
-let parsedUrl = url.parse(req.url);
-let pathName = parsedUrl.pathname;
-if(req.method === 'GET' && pathName === '/'  ){
-    res.write("Welcome to homepage")
-}else if( req.method === 'GET' && pathName === '/about'){
-    res.setHeader({'Content-Type' :'text/html'});
+// let parsedUrl = url.parse(req.url);
+// let pathName = parsedUrl.pathname;
+if(req.method === 'GET' && req.url === '/'  ){
+    res.end("Welcome to homepage")
+}else if( req.method === 'GET' && req.url === '/about'){
+    res.setHeader('Content-type' ,'text/html');
     res.end("<h2>this is all  about Node.js</h2>")
-}else if(req.method === 'POST' && pathName === '/about'){
-    res.setHeader({'Content-Type' :'text/plain'});
+}else if(req.method === 'POST' && req.url === '/about'){
+    res.setHeader('Content-type' ,'text/json');
     res.end(`{message: this is a post request}`)
 }else{
     res.end("No page")
